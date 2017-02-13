7e6da3107d7cc666a0ebc71966bbf0a598a03f5a
Nasir Uddin <nasiru32@gmail.com>
2017-02-13 18:40:08 +0100

    UI button added

--- Pitchmimick/ViewController.swift
+++ Pitchmimick/ViewController.swift
@@ -10,9 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordingLabel: UILabel!    
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var stopRecordingButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        stopRecordingButton.isEnabled = false
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("viewWillAppear called")
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,5 +31,16 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func recordAudio(_ sender: Any) {
+        recordingLabel.text = "Recording in progress"
+        stopRecordingButton.isEnabled = true
+        recordButton.isEnabled = false
+    }
+    
+    @IBAction func stopRecording(_ sender: Any) {
+        recordButton.isEnabled = true
+        stopRecordingButton.isEnabled = false
+        recordingLabel.text = "Tap to Record"
+    }
 }
 
---
+++ README.md
@@ -0,0 +1,4 @@
+# Pitchminick v 1
+Written by Nasir Uddin (https://www.linkedin.com/in/nasir-uddin-090a475/)
+- This is my version of Udacity pitchperfect app 
+-  This app records a user’s voice and then plays the modulated audio through a variety of filters (eg. voice like Darth Vader, Chipmunk etc)
